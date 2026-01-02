f6e192301fcbd9357213c5abc7ec681d6fb28371
rexchoppers <rexchoppers@gmail.com>
2026-01-02 08:48:08 +0000

    WIP Main files

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+import os
+
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 # Load environment variables
 load_dotenv()
 
+def main():
+    spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+    spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+    spotify_redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+        client_id=spotify_client_id,
+        client_secret=spotify_client_secret,
+        redirect_uri=spotify_redirect_uri,
+        scope="user-library-read,user-read-currently-playing,user-read-playback-state",
+    ))
+
+    # Get what's currently playing
+    results = sp.current_user_playing_track()
+    print(results)
 
 
+if __name__ == "__main__":
+    main()
